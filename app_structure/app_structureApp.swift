dff231d2622b8699d55fdfe8c29a2c4c2029b8f1
aleksei_rodimkin <alexeyrodimkin@gmail.com>
2026-03-02 02:59:30 +0300

    Initial Commit

---
+++ app_structure/app_structureApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_structureApp.swift
+//  app_structure
+//
+//  Created by Алексей Родимкин on 02.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct app_structureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
